fe79318103f63cf4139eafb374bda390dc509556
Brandon Hernández <isahecam@gmail.com>
2025-06-22 17:45:13 -0600

    fix(urls): update site URLs to use the correct domain and reference image from infoSite

--- src/data/info-site.ts
+++ src/data/info-site.ts
@@ -2,5 +2,5 @@ export const infoSite = {
   title: "isaheca.dev | My portfolio personal and learning journey ✨",
   description:
     "Welcome to my portfolio personal, here you can find my projects, experience and posts related to my work and my learning journey. ✨",
-  image: "https://isaheca.dev.vercel.app/og-isadevjs-links.webp",
+  image: "https://isaheca-dev.vercel.app/og-isadevjs-links.webp",
 };

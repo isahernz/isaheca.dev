a6f87323ec46d75636c4a2dc87b2500f0fccbe80
Brandon Hernández <isahecam@gmail.com>
2025-06-22 17:28:17 -0600

    feat(info-site): add infoSite data and update MainLayout with SEO metadata

---
+++ src/data/info-site.ts
@@ -0,0 +1,6 @@
+export const infoSite = {
+  title: "isaheca.dev | My portfolio personal and learning journey ✨",
+  description:
+    "Welcome to my portfolio personal, here you can find my projects, experience and posts related to my work and my learning journey. ✨",
+  image: "https://isaheca.dev.vercel.app/og-isadevjs-links.webp",
+};
